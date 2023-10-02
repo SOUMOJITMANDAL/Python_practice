2818d222b86061d1f15d0d5711187a5c8057945b
SOUMOJIT MANDAL <soumojit.jit@gmail.com>
2023-10-02 20:31:55 +0530

    First python code that prints sum of two numbers

--- Basics/Sum_of_num.py
+++ Basics/Sum_of_num.py
@@ -1,5 +1,5 @@
-x = int(input("Enter first number: ")) #Always takes a string value by default
-print(type(x)) # displays the type of datatype
+x = int(input("Enter first number: "))  # Always takes a string value by default
+print(type(x))  # displays the type of datatype
 y = int(input("Enter the second number: "))
-z = str(x + y) # z is now an int
-print("The sum of the numbers = " + z)
+z = str(x + y)  # z is now a string
+print("The sum of the numbers = " + z)  # only string can be concatenated with a string
